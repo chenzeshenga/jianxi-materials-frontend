a2699219ae70f558b43f0ce6b8332ecbcd1105f3
chenzeshenga <chenzeshenga@163.com>
2020-07-28 23:19:54 +0800

    样式更新

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -42,10 +42,43 @@
         </el-row>
         <el-row class="hidden-md-and-up">
             <el-carousel style="width: 100%;height: 200px" direction="vertical" :autoplay="true">
-                <el-carousel-item v-for="item in pics" :key="item">
-                    <el-image :src="item" fit="contain"></el-image>
+                <el-carousel-item>
+                    <img src="../assets/index/1.jpg" style="width: 100%" alt="建锡"/>
+                </el-carousel-item>
+                <el-carousel-item>
+                    <img src="../assets/index/2.jpg" style="width: 100%" alt="建锡"/>
+                </el-carousel-item>
+                <el-carousel-item>
+                    <img src="../assets/index/3.jpg" style="width: 100%" alt="建锡"/>
+                </el-carousel-item>
+                <el-carousel-item>
+                    <img src="../assets/index/4.jpg" style="width: 100%" alt="建锡"/>
+                </el-carousel-item>
+                <el-carousel-item>
+                    <img src="../assets/index/5.jpg" style="width: 100%" alt="建锡"/>
+                </el-carousel-item>
+                <el-carousel-item>
+                    <img src="../assets/index/6.jpg" style="width: 100%" alt="建锡"/>
                 </el-carousel-item>
             </el-carousel>
+            <div class="wrapper3" :style="wrapper">
+                <p style="z-index: 20;">公司理念</p>
+                <p style="z-index: 20;">创新、共赢、严谨、诚信</p>
+                <el-button
+                        style="text-align: left;margin-left: -32%"
+                        type="text" @click="route2introduce">
+                    了解更多
+                </el-button>
+            </div>
+            <div class="wrapper4" :style="wrapper2">
+                <p style="z-index: 20;">定位：特种合金靶材产业的拓荒者和耕耘者</p>
+                <p style="z-index: 20;">目标：全球知名的靶材及特种合金品牌</p>
+                <el-button
+                        style="white;text-align: left;margin-left: -32%"
+                        type="text" @click="route2introduce">
+                    了解更多
+                </el-button>
+            </div>
         </el-row>
     </div>
 </template>
@@ -55,14 +88,6 @@
         name: 'Home',
         data() {
             return {
-                pics: [
-                    'http://47.105.33.48:8889/img/1',
-                    'http://47.105.33.48:8889/img/2',
-                    'http://47.105.33.48:8889/img/3',
-                    'http://47.105.33.48:8889/img/4',
-                    'http://47.105.33.48:8889/img/5',
-                    'http://47.105.33.48:8889/img/6',
-                ],
                 hidden: {
                     display: 'none'
                 },
@@ -154,5 +179,25 @@
         color: white;
         margin-left: 10%;
         width: 52%
+    }
+
+    .wrapper3 {
+        animation: fade-in; /*动画名称*/
+        animation-duration: 1.5s; /*动画持续时间*/
+        -webkit-animation: fade-in 1.5s; /*针对webkit内核*/
+        z-index: 20;
+        margin-top: 12%;
+        border: black;
+        font-size: 20px;
+    }
+
+    .wrapper4 {
+        animation: fade-in; /*动画名称*/
+        animation-duration: 1.5s; /*动画持续时间*/
+        -webkit-animation: fade-in 1.5s; /*针对webkit内核*/
+        z-index: 20;
+        margin-top: 12%;
+        border: black;
+        font-size: 16px;
     }
 </style>
